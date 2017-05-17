ffa128209c556d78e613c498caccd2aaeb3c7601
Eric Traub <est2114@columbia.edu>
2017-05-16 22:26:43 -0400

    build min

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task('uglify', function() {
+gulp.task('min', function() {
 	gulp.src('tint.js')
 		.pipe(uglify())
 		.pipe(rename('tint.min.js'))
